c26e8503b3bbd967d4723653fbdbfe463efc5b66
Mandi Wise <hello@mandiwise.com>
2020-09-25 16:38:00 -0600

    Fix env variable name.

--- server/redis/index.js
+++ server/redis/index.js
@@ -2,6 +2,6 @@ const Redis = require("ioredis");
 
 require("./utils/streamTransformers");
 
-const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST);
+const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST_ADDRESS);
 
 module.exports = redis;
